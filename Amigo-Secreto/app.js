158b36700245d6a9d0e99a1485f25e7dcb5f099e
MoonLak3 <josemanugaga@gmail.com>
2025-08-17 21:52:15 -0600

    Skeleton for the app.js file has been created.

--- Amigo-Secreto/app.js
+++ Amigo-Secreto/app.js
@@ -1,1 +1,25 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+/*
+sorter for a friend list. The user has to be able to write the names
+of their friends, and it must display the names in a list. If the 
+name is empty, it should put out an alert, and not insert the name in
+the list. Afterwards, it has got to randomly select one of the names
+of the list
+*/
+
+// Variables
+let friends = []; //array where we'll store the name list
+let goobers = 0; //integer where we'll count all the names on the array (is not really needed, but is a nice to have) 
+let wizardry = 0; //integer dedicated to store the randomized number
+
+
+// Functions
+function rndm(){
+    
+}
+
+
+function MainApp(){
+
+}
+
+// Execution
