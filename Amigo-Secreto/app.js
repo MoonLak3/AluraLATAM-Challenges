836d6cfc4273d6c21378608b4217e624407e555a
MoonLak3 <josemanugaga@gmail.com>
2025-08-20 01:00:39 -0600

    Added multiple functions such as a way to restart the app and experimented with the html file

--- Amigo-Secreto/app.js
+++ Amigo-Secreto/app.js
@@ -13,13 +13,28 @@ let wizardry = 0; //integer dedicated to store the randomized number
 
 
 // Functions
-function rndm(){
-    
+function rndm(samplevalue){//function to generate a random number
+    let innerndm = Math.floor(Math.random() * samplevalue) + 1; //random number between 1 and user's inputted value
+    console.log(`User wizardry value: ${wizardry}`); 
+    return innerndm; //return the random number
 }
 
+function restartApp(){    //function to clear all the variables and reset the app
+    console.log("Restarting app...");
+    friends = [];
+    goobers = 0;
+    wizardry = 0;
+    document.getElementById("amigo").value = "";//clear textbox
+    document.getElementById("amigo").focus();//show cursor in textbox
+    document.getElementById("friend-list").innerHTML = "";//clear the list of friends
+    document.getElementById("result").innerHTML = "";//clear the sorting result
+}
 
 function MainApp(){
 
 }
 
 // Execution
+for(;;){
+    if
+}
